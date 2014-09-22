2c05ce39e88308cae76fc7c0779359665aa405ea
Denny Scott <dennyscott301@gmail.com>
2014-09-21 21:21:24 -0500

    fixed migrations

--- server/migrations.js
+++ server/migrations.js
@@ -2,10 +2,10 @@ Migrations.add({
   version: 1,
   name: 'All comments get a title.',
   up: function() {
-		Comments.update({}, {$set:{title: 'New Title'}});
+		Comments.update({}, {$set:{title: 'New Title'}}, {multi: true});
 	},
   down: function() {
-		Comments.update({}, {$unset:{title: true}});
+		Comments.update({}, {$unset:{title: true}}, {multi:true});
 	}
 });
 
